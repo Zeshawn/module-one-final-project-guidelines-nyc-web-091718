7a2c9f8a7b0fc3b6bb1e0ef67a1223672f400772
Zeshawn <zeshawnk@gmail.com>
2018-10-03 15:22:37 -0400

    Now displaying user artists

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,4 @@
 class Artist < ActiveRecord::Base
   has_many :users, through: :user_artists
+  has_many :user_artists
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
-  has_many :artists, through: :user_artist
+  has_many :artists, through: :user_artists
+  has_many :user_artists
 end
